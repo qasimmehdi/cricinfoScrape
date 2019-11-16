cd1304b88edd7bcd1d4c6b031821eb3c19ec1827
qasimmehdi <qasim_127@hotmail.com>
2019-11-16 23:25:52 +0500

    added Team selector

--- Fantasy Frontend/js/main.js
+++ Fantasy Frontend/js/main.js
@@ -1,5 +1,23 @@
-jQuery(document).ready(function($) {
+$(document).ready(function($) {
     $(".click-row").click(function() {
         window.location = $(this).data("href");
     });
 });
+
+$('#select-players tbody tr td span i').on('click', function (e) {
+    $(this).closest('i').removeClass('fa-plus-square');
+    $(this).closest('i').addClass('fa-minus-square');
+    var row = $(this).closest('tr').html();
+    $('#my-team tbody').append('<tr>'+row+'</tr>');
+    $(this).closest('tr').remove();
+});
+
+
+$('#my-team tbody tr td span i').on('click', function (e) {
+    $(this).closest('i').removeClass('fa-minus-square');
+    $(this).closest('i').addClass('fa-plus-square');
+    var row = $(this).closest('tr').html();
+    $('#select-players tbody').append('<tr>'+row+'</tr>');
+    $(this).closest('tr').remove();
+});
+
